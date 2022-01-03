a4fe02799f11a18b3030a28da4cae05b9437217f
sidneytrilogo <sidney@trilogo.com.br>
2022-01-03 01:33:40 -0300

    fixes helthchecks-ui

--- src/Apps/Argon.Zine.App.Api/Configurations/HealthChecksConfiguration.cs
+++ src/Apps/Argon.Zine.App.Api/Configurations/HealthChecksConfiguration.cs
@@ -1,9 +1,4 @@
-﻿using Argon.Restaurants.Infra.Data;
-using Argon.Zine.Catalog.Infra.Data;
-using Argon.Zine.Customers.Infra.Data;
-using Argon.Zine.Identity.Data;
-using Argon.Zine.Ordering.Infra.Data;
-using HealthChecks.UI.Client;
+﻿using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using RabbitMQ.Client;
 
@@ -13,33 +8,35 @@ public static class HealthChecksConfiguration
 {
     public static IServiceCollection RegisterHealthChecks(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddHealthChecks()
-            .AddDbContextCheck<CatalogContext>("Catalog Database", tags: new[] { "sql server" })
-            .AddDbContextCheck<CustomerContext>("Customer Database", tags: new[] { "sql server" })
-            .AddDbContextCheck<OrderingContext>("Ordering Database", tags: new[] { "sql server" })
-            .AddDbContextCheck<IdentityContext>("Identity Database", tags: new[] { "sql server" })
-            .AddDbContextCheck<RestaurantContext>("Restaurant Database", tags: new[] { "sql server" })
-            .AddMongoDb(mongodbConnectionString: configuration["BasketDatabaseSettings:ConnectionString"], name: "Basket Database", tags: new[] { "mongodb" })
-            .AddMongoDb(mongodbConnectionString: configuration["ChatDatabaseSettings:ConnectionString"], name: "Chat Database", tags: new[] { "mongodb" })
-            .AddRabbitMQ(provider => provider.GetRequiredService<IConnectionFactory>(), name: "Message Broker", tags: new[] { "rabbitmq" })
-            .AddRedis(configuration.GetConnectionString("CatalogRedis"), "Catalog Redis", tags: new[] { "redis" })
-            .AddEventStore(configuration.GetConnectionString("EventSourcingConnection"), name: "Event Sourcing Database", tags: new[] { "event store" });
+        var timeout = TimeSpan.FromSeconds(1);
 
+        services.AddHealthChecks()
+            .AddSqlServer(configuration.GetConnectionString("CatalogConnection"), name: "Catalog Database", tags: new[] { "sql server" }, timeout: timeout)
+            .AddSqlServer(configuration.GetConnectionString("CustomerConnection"), name: "Customer Database", tags: new[] { "sql server" }, timeout: timeout)
+            .AddSqlServer(configuration.GetConnectionString("OrderingConnection"), name: "Ordering Database", tags: new[] { "sql server" }, timeout: timeout)
+            .AddSqlServer(configuration.GetConnectionString("IdentityConnection"), name: "Identity Database", tags: new[] { "sql server" }, timeout: timeout)
+            .AddSqlServer(configuration.GetConnectionString("RestaurantConnection"), name: "Restaurant Database", tags: new[] { "sql server" }, timeout: timeout)
+            .AddMongoDb(mongodbConnectionString: configuration["BasketDatabaseSettings:ConnectionString"], name: "Basket Database", tags: new[] { "mongodb" }, timeout: timeout)
+            .AddMongoDb(mongodbConnectionString: configuration["ChatDatabaseSettings:ConnectionString"], name: "Chat Database", tags: new[] { "mongodb" }, timeout: timeout)
+            .AddRabbitMQ(provider => provider.GetRequiredService<IConnectionFactory>(), name: "Message Broker", tags: new[] { "rabbitmq" }, timeout: timeout)
+            .AddRedis(configuration.GetConnectionString("CatalogRedis"), "Catalog Redis", tags: new[] { "redis" }, timeout: timeout)
+            .AddEventStore(configuration.GetConnectionString("EventSourcingConnection"), name: "Event Sourcing Database", tags: new[] { "event store" }, timeout: timeout);
+        
         services.AddHealthChecksUI().AddSqliteStorage("Data Source=healthchecks.db");
 
-        return services;
+        return services;    
     }
 
     public static IApplicationBuilder UseHealthChecks(this IApplicationBuilder app)
     {
-        app.UseHealthChecks("/hc", new HealthCheckOptions
+        app.UseHealthChecks("/healthz", new HealthCheckOptions
         {
             Predicate = _ => true,
             ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
         }).UseHealthChecksUI(options =>
         {
-            options.ApiPath = "/hc";
-            options.UIPath = "/hc-ui";
+            options.UIPath = "/show-health-ui";
+            options.ApiPath = "/health-ui-api";
         });
 
         return app;
